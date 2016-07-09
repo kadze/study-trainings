2141dbbdfe23d9eb7a586e080c8e3706e2ef8e04
Andrey Shevtsov <ash@gamayun.sk>
2016-07-09 23:14:01 +0300

    try accessory view and editing style

---
+++ Study training/Study training/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Study training
+//
+//  Created by Andrey on 7/9/16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Study training/Study training/SAPViewController.h
@@ -0,0 +1,15 @@
+//
+//  SAPViewController.h
+//  Study training
+//
+//  Created by Andrey on 7/9/16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAPViewController : UIViewController <UITableViewDelegate, UITableViewDelegate>
+
+@property(nonatomic, strong) IBOutlet UITableView *tableView;
+
+@end
