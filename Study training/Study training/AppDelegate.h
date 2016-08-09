26830c6bdea3a214751138008016bd4b31ea8a1e
Andrey Shevtsov <ash@gamayun.sk>
2016-08-09 10:10:27 +0300

    project structure and property background color in app delegate. like a global variable

--- Study training/Study training/AppDelegate.h
+++ Study training/Study training/AppDelegate.h
@@ -9,9 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
+@property (nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong) UIColor *themeColor;
 
 @end
 
